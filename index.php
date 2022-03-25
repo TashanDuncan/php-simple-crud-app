a1a7c6e5bbf4774115c615fd33f4e24398b112ed
Tashan Duncan <tashanduncan1994@gmail.com>
2022-03-25 10:01:27 +0000

    add buttons on user table

--- index.php
+++ index.php
@@ -25,18 +25,24 @@ $users = getUsers();
                 <th>Email</th>
                 <th>Phone</th>
                 <th>Website</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($users as $user) :?>
+            <?php foreach ($users as $user) : ?>
                 <tr>
-                    <td><?php echo $user['name']?></td>
-                    <td><?php echo $user['username']?></td>
-                    <td><?php echo $user['email']?></td>
-                    <td><?php echo $user['phone']?></td>
-                    <td><?php echo $user['website']?></td>
+                    <td><?php echo $user['name'] ?></td>
+                    <td><?php echo $user['username'] ?></td>
+                    <td><?php echo $user['email'] ?></td>
+                    <td><?php echo $user['phone'] ?></td>
+                    <td><?php echo $user['website'] ?></td>
+                    <td>
+                        <a href="view.php?id=<?php echo $user['id'] ?>" class="btn  btn-sm btn-outline-info">View</a>
+                        <a href="update.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
+                        <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                    </td>
                 </tr>
-                <?php endforeach;; ?>
+            <?php endforeach;; ?>
         </tbody>
     </table>
 
