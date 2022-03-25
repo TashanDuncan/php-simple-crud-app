869f9d144361b44fb8b1142613fb28370d877303
Tashan Duncan <tashanduncan1994@gmail.com>
2022-03-25 09:56:37 +0000

    display data in table

--- index.php
+++ index.php
@@ -1,12 +1,45 @@
+<?php
+require 'users.php';
+
+$users = getUsers();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Simple PHP crud APP</title>
 </head>
+
 <body>
-    <
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>UserName</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Website</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user) :?>
+                <tr>
+                    <td><?php echo $user['name']?></td>
+                    <td><?php echo $user['username']?></td>
+                    <td><?php echo $user['email']?></td>
+                    <td><?php echo $user['phone']?></td>
+                    <td><?php echo $user['website']?></td>
+                </tr>
+                <?php endforeach;; ?>
+        </tbody>
+    </table>
+
 </body>
+
 </html>
--- users.php
+++ users.php
@@ -2,5 +2,21 @@
 
 function getUsers()
 {
-    
+    return json_decode(file_get_contents(__DIR__ . '/users.json'), true);
+}
+
+function getUserById($id)
+{
+}
+
+function createUser($data)
+{
+}
+
+function updateUser($data, $id)
+{
+}
+
+function deleteUser($id)
+{
 }
